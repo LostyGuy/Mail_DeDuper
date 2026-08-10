fa05ab7286c8179839c901546ae6df262b7da0fb
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2026-08-10 15:29:52 +0200

    Implement move_duplicates functionality and raise NotImplementedError for REMOVE_MODE

--- Mail_DeDuper/main.py
+++ Mail_DeDuper/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from src.mail_deduper.app.load_config import load_environmental_values
 from src.mail_deduper.app.gather_emails import get_shared_email, get_users_inboxes, get_users_emails
 from src.mail_deduper.app.find_duplicates import find_dupes
+from src.mail_deduper.app.move_duplicates import start_move_sequence
 from Mail_DeDuper.src.mail_deduper.app.load_config import assign_environmental_to_variables as lc
 from src.mail_deduper.logs import log_error
 
@@ -20,9 +21,9 @@ if __name__ == "__main__":
     find_dupes()
     
     if lc.REMOVE_MODE == "TRUE":
-        ...
+        raise NotImplementedError
     elif lc.MOVE_MODE == "TRUE":
-        ...
+        start_move_sequence()
     else:
         log_error("main.py: ", "Invalid Value Has Been Passed. \n", f"REMOVE_MODE: {lc.REMOVE_MODE} \n", f"MOVE_MODE: {lc.MOVE_MODE} \n")
         raise ValueError("Invalid Value Has Been Passed")
